b3818422a00834368d5b62669056dc6a96c302f0
ameypd <h20220013@goa.bits-pilani.ac.in>
2023-05-16 22:16:13 +0530

    secret access

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
   return "Hello, World!"
+
+@app.route('/sec')
+def hello_world():
+    my_secret = os.environ.get('MY_SECRET')
+    return f'My secret is: {my_secret}'
 
 if __name__ == "__main__":
     from waitress import serve
